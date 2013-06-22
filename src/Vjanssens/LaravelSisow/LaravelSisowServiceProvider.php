f35b2d79e7bd3796a329f321d4c66669e67328fa
Vadiem <vadiemjanssens@gmail.com>
2013-06-22 14:33:49 +0200

    Initial commit

---
+++ src/Vjanssens/LaravelSisow/LaravelSisowServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Vjanssens\LaravelSisow;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSisowServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('vjanssens/laravel-sisow');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
